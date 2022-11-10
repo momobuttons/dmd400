8d110922f0b756dea9475d2d07ad05927da35355
poofsjawn <poofsjawn@gmail.com>
2022-11-09 23:37:52 -0500

    camera and object placement ish

--- blob/main.js
+++ blob/main.js
@@ -15,18 +15,18 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 //light
 const light = new THREE.PointLight(0xffffff, 2);
-light.position.set(10, 10, 10);
+light.position.set(0, 0, 0);
 scene.add(light);
 
 let clock = new THREE.Clock();
 
 
-
 //camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
-camera.position.z = 5;
-camera.position.x = 1;
-camera.position.y = 1;
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000);
+
+var helper = new THREE.CameraHelper(camera);
+scene.add(helper);
+
 //renderer
 const renderer = new THREE.WebGL1Renderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -42,11 +42,7 @@ const group = new THREE.Group();
 const jargroup = new THREE.Group();
 const toptop = new THREE.Group();
 
-//light position gui
-//gui.add(light.position,'x').min(0).max(100).name('Light position X');
-//gui.add(light.position,'y').min(0).max(100).name('Light position Y');
-//gui.add(light.position,'z').min(0).max(100).name('Light position Z');
-//bottom jar
+
 loader.load( './blender/jarbottomSmoothLG.glb', function ( gltf ) {
     let model = gltf.scene;
     //glass
@@ -72,35 +68,30 @@ loader.load( './blender/jarbottomSmoothLG.glb', function ( gltf ) {
 
 //make the file name random on start --> eventually on burn 
 
-let img1 = 'vrina.glb';
-let img2 = 'salComune.glb';
-let img3 = 'jgms3.glb';
-let img4 = 'jynistoe.glb';
-let img5 = 'oleum.glb';
+let img1 = 'vrinar.glb';
+let img2 = 'salComunerr.glb';
+let img3 = 'jgmsr.glb';
+let img4 = 'jynistoer2.glb';
+let img5 = 'oleumr.glb';
 const symbol = [img1 , img2, img3, img4, img5];
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 //middle jar
-loader.load( `./blender/topRandom/${symbol[getRandomInt(4)]}`, function ( gltf ) {
-    let modelTwo = gltf.scene
+loader.load( `./blender/topRandom/${symbol[2]}`, function ( gltf ) {
+    let modelTwo = gltf.scene;
     group.add(modelTwo);
     jargroup.add(modelTwo);
     //toptop.add(modelTwo);
 }, undefined, function ( error ) {
     console.error( error );
 });
-//gui.add(toptop.rotation,'x', -100, 100, .1).name('toptop position X');
-gui.add(jargroup.rotation,'y', -100, 100, .1).name('jargroup position y');
-//gui.add(toptop.rotation,'z', -100, 100, .1).name('jargroup position z');
-//jargroup.rotation.y = 3.6;
-//top jar
-loader.load( './blender/toptop2.glb', function ( gltf ) {
-    let modelThree = gltf.scene
+
+loader.load( './blender/toptop5.glb', function ( gltf ) {
+    let modelThree = gltf.scene;
     group.add(modelThree);
     jargroup.add(modelThree);
-   // toptop.add(modelThree);
 }, undefined, function ( error ) {
     console.error( error );
 });
@@ -110,13 +101,7 @@ loader.load( './blender/toptop2.glb', function ( gltf ) {
 //character
 loader.load( './blender/dudeLG.glb', function ( gltf ) {
     let modeldude = gltf.scene;
-    //gui.add(modeldude.position,'x', 0, 10, .1).name('character position X');
-   /* gui.add(modeldude.position,'y', -10, 10, .1).name('character position Y');
-    gui.add(modeldude.position,'z', 0, 10, .1).name('character position Z');
 
-    gui.add(modeldude.rotation,'x', 0, 10, .1).name('character position X');
-    gui.add(modeldude.position,'y', -10, 10, .1).name('character position Y');
-    gui.add(modeldude.position,'z', 0, 10, .1).name('character position Z');*/
     group.add(modeldude);
     //jargroup.add(modeldude);
 }, undefined, function ( error ) {
@@ -128,44 +113,15 @@ controls.update();
 scene.add(group);
 scene.add(toptop);
 
-
-
-
-//group.position.z= 40;
-//group.rotation.y = 40;
-//gui.add(light3.position,'x', -500, 500, .0001).name('group position X');
 light.position.y = 72;
 light.position.z = 50;
 light.position.x = 5;
-/*gui.add(group.position,'y', -10, 10, .1).name('group position Y');
-gui.add(group.position,'z', 0, 10, .1).name('group position Z');
-gui.add(group.rotation,'x', 0, 50, .1).name('group rotation X');
-gui.add(group.rotation,'y', 0, 50, .1).name('group rotation Y');
-gui.add(group.rotation,'z', 0, 50, .1).name('group rotation Z');*/
+
 scene.add(jargroup);
-jargroup.position.x= 0;
-jargroup.position.z= 12;
-jargroup.position.y= -.3;
-jargroup.rotation.y = 5;
 
-
-
-group.position.x= 0;
-group.position.z= 12;
-group.position.y= 2;
-group.rotation.y = 5;
 camera.position.z = 20;
 camera.position.x = 0;
-camera.position.y = 0.3;
-
-/*gui.add(jargroup.position,'x', 0, 10, .1).name('jargroup position X');
-gui.add(jargroup.position,'y', -10, 10, .1).name('jargroup position Y');
-gui.add(jargroup.position,'z', 0, 10, .1).name('jargroup position Z');
-gui.add(jargroup.rotation,'x', 0, 50, .1).name('jargroup rotation X');
-gui.add(jargroup.rotation,'y', 0, 50, .1).name('jargroup rotation Y');
-gui.add(jargroup.rotation,'z', 0, 50, .1).name('jargroup rotation Z');*/
-//camera
-
+camera.position.y = 0;
 
 
 function animate() {
--- main.js
+++ main.js
@@ -25,7 +25,8 @@ camera.position.set( 10, 10, 10 );
 renderer.render( scene, camera);
 
 
-/*let posX = 15;
+/*
+let posX = 15;
 let posY = 32;
 let posZ = 16;
 const geometry = new THREE.SphereBufferGeometry( posX, posY, posZ );
@@ -37,8 +38,8 @@ sphere.material.color.set(color3);
 
 scene.add( sphere );
 */
-scene.add(new THREE.AxesHelper(5));
 
+scene.add(new THREE.AxesHelper(5));
 
 const geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
 const material = new THREE.MeshPhysicalMaterial({
@@ -51,6 +52,7 @@ const material = new THREE.MeshPhysicalMaterial({
   opacity: 1,
   reflectivity: 0.2,
 })
+
 const cylinder = new THREE.Mesh( geometry, material );
 scene.add( cylinder );
 cylinder.position.set(10, 10, 10);
@@ -63,7 +65,7 @@ const sphere = new THREE.Mesh( geometry1, material1 );
 scene.add( sphere );
 sphere.position.set(10, 10, 10);
 
-  // Get float array of all coordinates of vertices
+   // Get float array of all coordinates of vertices
   //const float32array = geometry.attributes.position.array;
   // run loop,  each step of loop need increment by 3, because each vertex has 3 coordinates, X, Y and Z
  /* for (let i = 0; i < float32array.length; i += 3) {
@@ -72,10 +74,6 @@ sphere.position.set(10, 10, 10);
     console.log(float32array[i]); // Y coordinate
     console.log(float32array[i]); // Z coordinate
   }*/
-
-
-
-
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
